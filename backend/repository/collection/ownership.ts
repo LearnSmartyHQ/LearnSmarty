9e26a12568b1b4a113adee98118f92785c7b4182
MrAdib <Mr.John.Adib@gmail.com>
2023-11-06 00:17:53 +0000

    improve ownership

--- backend/repository/collection/ownership.ts
+++ backend/repository/collection/ownership.ts
@@ -2,7 +2,16 @@ import ForbiddenError from '@errors/forbidden-error';
 import type { IUser } from '@repository/user/interfaces/user-interface';
 import type { ICollection } from './interfaces/collection-interface';
 
-export const checkCollectionOwnership = (user: IUser, collection: ICollection)
+interface IOwnershipCollection {
+  user: IUser;
+  collection: ICollection;
+}
+
+// export const checkCollectionOwnership = (user: IUser, collection: ICollection)
+export const checkCollectionOwnership = ({
+  user,
+  collection,
+}: IOwnershipCollection)
 : void => {
   if (collection.ownerId !== user.id) {
     throw new ForbiddenError('Unauthorized access: you have no permission to access this data.');
