2fbeccf86f0234488d83f88112ed0f7b005ff1eb
MrAdib <Mr.John.Adib@gmail.com>
2023-11-06 00:54:31 +0000

    fix import

--- app/api/collections/route.ts
+++ app/api/collections/route.ts
@@ -1,5 +1,5 @@
 import type { IApi } from '@api/_interface/api-response';
-import { getAllCollections } from 'backend/collection/repository/getAllCollections';
+import { getAllCollections } from '@collection/repository/getAllCollections';
 
 import { NextResponse } from 'next/server';
 
